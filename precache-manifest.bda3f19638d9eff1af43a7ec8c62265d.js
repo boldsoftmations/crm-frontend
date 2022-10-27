1106f8cacd7cbf79a3e624f15662a88f32f5e09e
vivek-boldSoftMations <vivek@glutape.com>
2022-10-27 15:38:33 +0530

    Updates

---
+++ precache-manifest.bda3f19638d9eff1af43a7ec8c62265d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9f1f008bb64f79292c0",
+    "url": "/crm-frontend/static/css/main.d1a06b09.chunk.css"
+  },
+  {
+    "revision": "a9f1f008bb64f79292c0",
+    "url": "/crm-frontend/static/js/main.17e76378.chunk.js"
+  },
+  {
+    "revision": "68facbb7e7f2fb0f4ca0",
+    "url": "/crm-frontend/static/js/runtime~main.c21cafe3.js"
+  },
+  {
+    "revision": "5e5d5112945801207ec5",
+    "url": "/crm-frontend/static/css/2.8899bf73.chunk.css"
+  },
+  {
+    "revision": "5e5d5112945801207ec5",
+    "url": "/crm-frontend/static/js/2.d6d23b30.chunk.js"
+  },
+  {
+    "revision": "7d2474989e7fd07a6e13f4479e2b7dff",
+    "url": "/crm-frontend/index.html"
+  }
+];
