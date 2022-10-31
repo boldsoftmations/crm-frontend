678309bc3c2763648aaad8b2732ade472a3ddeb1
vivek-boldSoftMations <vivek@glutape.com>
2022-10-31 15:10:07 +0530

    Updates

---
+++ precache-manifest.9539c9f7e3d32a9758f7e82fd91db370.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "912ef095e9e8513d6853",
+    "url": "/crm-frontend/static/css/main.a2797432.chunk.css"
+  },
+  {
+    "revision": "912ef095e9e8513d6853",
+    "url": "/crm-frontend/static/js/main.c878b74f.chunk.js"
+  },
+  {
+    "revision": "68facbb7e7f2fb0f4ca0",
+    "url": "/crm-frontend/static/js/runtime~main.c21cafe3.js"
+  },
+  {
+    "revision": "20f80868aed8a3917917",
+    "url": "/crm-frontend/static/css/2.8899bf73.chunk.css"
+  },
+  {
+    "revision": "20f80868aed8a3917917",
+    "url": "/crm-frontend/static/js/2.d258dade.chunk.js"
+  },
+  {
+    "revision": "66b1e7da908a64d10c2406aabe25a186",
+    "url": "/crm-frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/crm-frontend/precache-manifest.4ceb71ba6386eefaca393cb03acf68cf.js"
+  "/crm-frontend/precache-manifest.9539c9f7e3d32a9758f7e82fd91db370.js"
 );
 
 workbox.clientsClaim();
