13323115c2a91288dd490949761e6d3ff0a7fcd2
vivek-boldSoftMations <vivek@glutape.com>
2022-11-17 17:25:51 +0530

    fix : set default inida in contact field leads

--- src/Pages/Leads/UpdateLeads.jsx
+++ src/Pages/Leads/UpdateLeads.jsx
@@ -92,8 +92,8 @@ export const UpdateLeads = (props) => {
   const [personName, setPersonName] = useState([]);
   const [phone, setPhone] = useState();
   const [phone2, setPhone2] = useState();
-  const [contacts1, setContacts1] = useState('');
-  const [contacts2, setContacts2] = useState('');
+  const [contacts1, setContacts1] = useState("");
+  const [contacts2, setContacts2] = useState("");
   const handlePhoneChange = (newPhone) => {
     setPhone(newPhone);
   };
@@ -168,14 +168,13 @@ export const UpdateLeads = (props) => {
     }
   };
 
-
   const updateLeadsData = async (e) => {
     if (activeStep === steps.length - 1) {
       try {
         e.preventDefault();
         setOpen(true);
-        const contact1 = phone !== undefined  ? `+${phone}` : contacts1  ;
-        const contact2 = phone2 !== undefined  ? `+${phone2}` : contacts2 ;
+        const contact1 = phone !== undefined ? `+${phone}` : contacts1;
+        const contact2 = phone2 !== undefined ? `+${phone2}` : contacts2;
         const data = {
           name: leads.name,
           alternate_contact_name: leads.altContactName
@@ -183,7 +182,7 @@ export const UpdateLeads = (props) => {
             : "",
           email: leads.email ? leads.email : "",
           alternate_email: leads.alternate_email ? leads.alternate_email : "",
-          contact:  contact1 ,
+          contact: contact1,
           alternate_contact: contact2,
           description: descriptionValue,
           target_date: leads.target_date,
@@ -299,7 +298,7 @@ export const UpdateLeads = (props) => {
                         minWidth: "500px",
                       }}
                       country={"in"}
-                      value={phone  ? `+${phone}` : contacts1}
+                      value={phone ? `+${phone}` : contacts1 ? contacts1 : ""}
                       onChange={handlePhoneChange}
                     />
                   </Grid>
@@ -312,7 +311,7 @@ export const UpdateLeads = (props) => {
                         minWidth: "500px",
                       }}
                       country={"in"}
-                      value={phone2  ? `+${phone2}` : contacts2}
+                      value={phone2 ? `+${phone2}` : contacts2 ? contacts2 : ""}
                       onChange={handlePhoneChange2}
                     />
                   </Grid>
@@ -591,8 +590,7 @@ export const UpdateLeads = (props) => {
                     Contact : {phone ? `+${phone}` : contacts1}
                   </Grid>
                   <Grid item xs={12} sm={6}>
-                    Alt. Contact :{" "}
-                    {phone2 ? `+${phone2}` : contacts2}
+                    Alt. Contact : {phone2 ? `+${phone2}` : contacts2}
                   </Grid>
                   <Grid item xs={12} sm={6}>
                     Busniess Type : {businesTypes}
