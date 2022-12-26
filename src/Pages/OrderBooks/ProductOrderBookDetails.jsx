745221e071706f2d573b2f9bf89cb6ccca61a5c3
vivek-boldSoftMations <vivek@glutape.com>
2022-12-26 16:42:38 +0530

    feat : added order book

---
+++ src/Pages/OrderBooks/ProductOrderBookDetails.jsx
@@ -0,0 +1,257 @@
+import React, { useState, useEffect, useRef } from "react";
+import InvoiceServices from "../../services/InvoiceService";
+import {
+  Backdrop,
+  CircularProgress,
+  styled,
+  Table,
+  TableBody,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableCell,
+  Button,
+  Box,
+  Paper,
+  Grid,
+} from "@mui/material";
+import { tableCellClasses } from "@mui/material/TableCell";
+import { CSVLink } from "react-csv";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
+const headers = [
+
+  {
+    label: "Product",
+    key: "product",
+  },
+  {
+    label: "Quantity",
+    key: "quantity",
+  },
+  {
+    label: "Amount",
+    key: "amount",
+  },
+  {
+    label: "Pending Quantity",
+    key: "pending_quantity",
+  },
+  { label: "Customer", key: "company" },
+  { label: "Billing City", key: "billing_city" },
+  { label: "Shipping City", key: "shipping_city" },
+];
+
+
+export const ProductOrderBookDetails = () => {
+  const [orderBookData, setOrderBookData] = useState([]);
+  const errRef = useRef();
+  const [open, setOpen] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+
+  useEffect(() => {
+    getAllLeadsPIDetails();
+  }, []);
+
+  const getAllLeadsPIDetails = async () => {
+    try {
+      setOpen(true);
+      const response = await InvoiceServices.getOrderBookProductsData();
+      setOrderBookData(response.data.results);
+      //   const total = response.data.count;
+      //   setpageCount(Math.ceil(total / 25));
+      setOpen(false);
+    } catch (err) {
+      setOpen(false);
+      if (!err.response) {
+        setErrMsg(
+          "“Sorry, You Are Not Allowed to Access This Page” Please contact to admin"
+        );
+      } else if (err.response.status === 400) {
+        setErrMsg(
+          err.response.data.errors.name
+            ? err.response.data.errors.name
+            : err.response.data.errors.non_field_errors
+        );
+      } else if (err.response.status === 401) {
+        setErrMsg(err.response.data.errors.code);
+      } else {
+        setErrMsg("Server Error");
+      }
+      errRef.current.focus();
+    }
+  };
+
+  const data = orderBookData.map((item) => ({
+    product: item.product,
+    quantity: item.quantity,
+    amount: item.amount,
+    pending_quantity: item.pending_quantity,
+    company: item.orderbook.company,
+    billing_city: item.orderbook.billing_city,
+    shipping_city: item.orderbook.shipping_city,
+  }));
+
+  console.log(data);
+
+  console.log("data :>> ", data);
+
+  return (
+    <div>
+      {" "}
+      <div>
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={open}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      </div>
+      <Grid item xs={12}>
+        <p
+          style={{
+            width: "100%",
+            padding: 10,
+            marginBottom: 10,
+            borderRadius: 4,
+            backgroundColor: errMsg ? "red" : "offscreen",
+            textAlign: "center",
+            color: "white",
+            textTransform: "capitalize",
+          }}
+          ref={errRef}
+          className={errMsg ? "errmsg" : "offscreen"}
+          aria-live="assertive"
+        >
+          {errMsg}
+        </p>
+        <Paper sx={{ p: 2, m: 4, display: "flex", flexDirection: "column" }}>
+          <Box display="flex">
+            <Box flexGrow={2}></Box>
+            <Box flexGrow={2}>
+              <h3
+                style={{
+                  textAlign: "left",
+                  marginBottom: "1em",
+                  fontSize: "24px",
+                  color: "rgb(34, 34, 34)",
+                  fontWeight: 800,
+                }}
+              >
+                Product Order Book Details
+              </h3>
+            </Box>
+            <Box flexGrow={0.5}>
+              <CSVLink
+                data={data}
+                headers={headers}
+                filename={"my-file.csv"}
+                target="_blank"
+                style={{
+                  textDecoration: "none",
+                  outline: "none",
+                  height: "5vh",
+                }}
+              >
+                <Button variant="contained">Export to Excel</Button>
+              </CSVLink>
+            </Box>
+          </Box>
+          <TableContainer sx={{ maxHeight: 440 }} component={Paper}>
+            <Table
+              sx={{ minWidth: 1200 }}
+              stickyHeader
+              aria-label="collapsible table"
+            >
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell align="center">PRODUCT</StyledTableCell>
+                  <StyledTableCell align="center">QUANTITY</StyledTableCell>
+                  <StyledTableCell align="center">AMOUNT</StyledTableCell>
+            
+                  <StyledTableCell align="center">
+                    PENDING QUANTITY
+                  </StyledTableCell>
+                  <StyledTableCell align="center">COMPANY</StyledTableCell>
+                  <StyledTableCell align="center">BILLING CITY</StyledTableCell>
+                  <StyledTableCell align="center">SHIPPING CITY</StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {orderBookData.map((row) => (
+                  <StyledTableRow
+                    key={row.id}
+                    sx={{ "& > *": { borderBottom: "unset" } }}
+                  >
+                    {/* <StyledTableCell>
+                      <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => setTableExpand(!tableExpand)}
+                      >
+                        {tableExpand ? (
+                          <KeyboardArrowUpIcon />
+                        ) : (
+                          <KeyboardArrowDownIcon />
+                        )}
+                      </IconButton>
+                    </StyledTableCell> */}
+                    <StyledTableCell align="center">
+                      {row.product}
+                    </StyledTableCell>
+                    <StyledTableCell align="center">
+                      {row.quantity}
+                    </StyledTableCell>
+                    <StyledTableCell align="center">
+                      {row.amount}
+                    </StyledTableCell>
+                    <StyledTableCell align="center">
+                      {row.pending_quantity}
+                    </StyledTableCell>
+                    <StyledTableCell align="center">
+                        {row.orderbook.company}
+                     </StyledTableCell>
+                     <StyledTableCell align="center">{row.orderbook.billing_city}</StyledTableCell>
+                     <StyledTableCell align="center">
+                     {row.orderbook.shipping_city}
+                      </StyledTableCell>
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          {/* <TableFooter
+            sx={{ display: "flex", justifyContent: "center", marginTop: "2em" }}
+          >
+            <Pagination
+              count={pageCount}
+              onChange={handlePageClick}
+              color={"primary"}
+              variant="outlined"
+              shape="circular"
+            />
+          </TableFooter> */}
+        </Paper>
+      </Grid>
+    </div>
+  );
+};
