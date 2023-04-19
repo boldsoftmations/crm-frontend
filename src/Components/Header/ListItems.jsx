9d681d1089d51ddedf4e5c52ade3821d632669a6
vivek-boldSoftMations <vivek@glutape.com>
2023-04-19 17:58:25 +0530

    fix : remove bill of material from inventory to accounts

--- src/Components/Header/ListItems.jsx
+++ src/Components/Header/ListItems.jsx
@@ -3344,19 +3344,7 @@ export const ListItems = (props) => {
                       primary="Production Inventory (Cons)"
                     />
                   </ListItem>
-                  <ListItem
-                    button
-                    component={RouterLink}
-                    to="/inventory/view-bill-of-materials"
-                    style={{ width: 300 }}
-                  >
-                    <ListItemText
-                      component={Button}
-                      onClick={() => setOpen(false)}
-                      inset
-                      primary="Bill of Materials"
-                    />
-                  </ListItem>
+
                   <ListItem
                     button
                     component={RouterLink}
