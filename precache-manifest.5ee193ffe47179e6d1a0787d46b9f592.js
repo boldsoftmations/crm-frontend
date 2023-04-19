2d3e70e6e22695ea459b33c712a5f9d4dd0124d5
vivek-boldSoftMations <vivek@glutape.com>
2023-04-19 18:00:53 +0530

    Updates

---
+++ precache-manifest.5ee193ffe47179e6d1a0787d46b9f592.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a5fdd63662082c70882",
+    "url": "/crm-frontend/static/css/main.635baaee.chunk.css"
+  },
+  {
+    "revision": "8a5fdd63662082c70882",
+    "url": "/crm-frontend/static/js/main.8124a557.chunk.js"
+  },
+  {
+    "revision": "77e7c2c15b0c14a5189b",
+    "url": "/crm-frontend/static/js/runtime~main.1db53189.js"
+  },
+  {
+    "revision": "b6465841ab14d225cb0e",
+    "url": "/crm-frontend/static/css/2.aec6d9c2.chunk.css"
+  },
+  {
+    "revision": "b6465841ab14d225cb0e",
+    "url": "/crm-frontend/static/js/2.912155c6.chunk.js"
+  },
+  {
+    "revision": "8c4926438a2fbd76ace9",
+    "url": "/crm-frontend/static/js/3.9ba52f05.chunk.js"
+  },
+  {
+    "revision": "16b798f596b5a691a945",
+    "url": "/crm-frontend/static/js/4.8a9e8cd4.chunk.js"
+  },
+  {
+    "revision": "8b082be98cec6497ef1d",
+    "url": "/crm-frontend/static/js/5.126f7b06.chunk.js"
+  },
+  {
+    "revision": "ba6cbe1188164e4fdba00e0c569bd5ce",
+    "url": "/crm-frontend/static/media/LOGOS3.ba6cbe11.png"
+  },
+  {
+    "revision": "92da350d0151010c0b2e7f615099d927",
+    "url": "/crm-frontend/static/media/ISOLogo.92da350d.ico"
+  },
+  {
+    "revision": "cc6c45777bc3ce4315ae1fbe24b1fba9",
+    "url": "/crm-frontend/static/media/allLogo.cc6c4577.jpg"
+  },
+  {
+    "revision": "185e3428466aee59cf6faf01edf54dfd",
+    "url": "/crm-frontend/static/media/MSME.185e3428.jpeg"
+  },
+  {
+    "revision": "5ef5f772b0579320f5a789ab8cb39dd2",
+    "url": "/crm-frontend/static/media/browser.5ef5f772.cjs"
+  },
+  {
+    "revision": "4fdeb5ddad38ad30c669df35cd5222d9",
+    "url": "/crm-frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/crm-frontend/precache-manifest.56baf6a78ed0a02be2710acbe2605134.js"
+  "/crm-frontend/precache-manifest.5ee193ffe47179e6d1a0787d46b9f592.js"
 );
 
 workbox.clientsClaim();
