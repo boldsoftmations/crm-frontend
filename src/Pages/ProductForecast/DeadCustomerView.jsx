fd1f1544a248465418ffbe246667767b10b92950
vivek-boldSoftMations <vivek@glutape.com>
2023-05-02 19:36:46 +0530

    fix : assignedto circle in dead customer

--- src/Pages/ProductForecast/DeadCustomerView.jsx
+++ src/Pages/ProductForecast/DeadCustomerView.jsx
@@ -396,19 +396,23 @@ export const DeadCustomerView = () => {
                       {row.state}
                     </StyledTableCell>
                     <StyledTableCell align="center">
-                    {row.assigned_to && row.assigned_to.length > 0 && (
-  <div
-    style={{
-      border: "1px solid #4caf50",
-      borderRadius: "20px",
-      padding: "4px 8px",
-      color: "#4caf50",
-    }}
-  >
-    {row.assigned_to.map((assignedTo) => assignedTo).join(", ")}
-  </div>
-)}
-
+                      {row.assigned_to.map((assignedTo) => {
+                        return (
+                          row.assigned_to &&
+                          row.assigned_to.length > 0 && (
+                            <div
+                              style={{
+                                border: "1px solid #4caf50",
+                                borderRadius: "20px",
+                                padding: "4px 8px",
+                                color: "#4caf50",
+                              }}
+                            >
+                              {assignedTo}
+                            </div>
+                          )
+                        );
+                      })}
                     </StyledTableCell>
                     <StyledTableCell align="center">
                       {row.contacts && row.contacts[0]
