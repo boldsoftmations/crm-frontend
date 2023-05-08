d0d88ce71d770dcaf8c6baf876f0c7a5ae2dfd74
vivek-boldSoftMations <vivek@glutape.com>
2023-05-08 11:19:09 +0530

    fix : basename from crm to crm-frontend

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename="/crm">
+    <BrowserRouter basename="/crm-frontend">
       <Header />
       <div className="appcontainer">
         <RouteScreen />
