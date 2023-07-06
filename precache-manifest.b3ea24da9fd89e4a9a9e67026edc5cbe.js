973125c0341df9d7b2443e287daaad4ab8e7b375
vivek-boldSoftMations <vivek@glutape.com>
2023-07-06 11:27:18 +0530

    Updates

---
+++ precache-manifest.b3ea24da9fd89e4a9a9e67026edc5cbe.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "f44b6dcdff2be7308781",
+    "url": "/crm-frontend/static/css/main.dd78bc75.chunk.css"
+  },
+  {
+    "revision": "f44b6dcdff2be7308781",
+    "url": "/crm-frontend/static/js/main.aeac258e.chunk.js"
+  },
+  {
+    "revision": "d594649d4b751af28580",
+    "url": "/crm-frontend/static/js/runtime~main.6e26508a.js"
+  },
+  {
+    "revision": "77444e6d7ef50a9c2a7d",
+    "url": "/crm-frontend/static/css/2.aec6d9c2.chunk.css"
+  },
+  {
+    "revision": "77444e6d7ef50a9c2a7d",
+    "url": "/crm-frontend/static/js/2.c8aee295.chunk.js"
+  },
+  {
+    "revision": "20d1e62cced25ddaa830",
+    "url": "/crm-frontend/static/js/3.157c517b.chunk.js"
+  },
+  {
+    "revision": "6e02967acef1cbc64c98",
+    "url": "/crm-frontend/static/js/4.0c388c65.chunk.js"
+  },
+  {
+    "revision": "c30536d3bf56ae1f485e",
+    "url": "/crm-frontend/static/js/5.09559c01.chunk.js"
+  },
+  {
+    "revision": "ba6cbe1188164e4fdba00e0c569bd5ce",
+    "url": "/crm-frontend/static/media/LOGOS3.ba6cbe11.png"
+  },
+  {
+    "revision": "cc6c45777bc3ce4315ae1fbe24b1fba9",
+    "url": "/crm-frontend/static/media/allLogo.cc6c4577.jpg"
+  },
+  {
+    "revision": "185e3428466aee59cf6faf01edf54dfd",
+    "url": "/crm-frontend/static/media/MSME.185e3428.jpeg"
+  },
+  {
+    "revision": "92da350d0151010c0b2e7f615099d927",
+    "url": "/crm-frontend/static/media/ISOLogo.92da350d.ico"
+  },
+  {
+    "revision": "5ef5f772b0579320f5a789ab8cb39dd2",
+    "url": "/crm-frontend/static/media/browser.5ef5f772.cjs"
+  },
+  {
+    "revision": "293088e28ee37ad39c6c0572c29286e6",
+    "url": "/crm-frontend/static/media/ISO.293088e2.png"
+  },
+  {
+    "revision": "c51af771bd7f6ab90b2e7ab1a1fa9953",
+    "url": "/crm-frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/crm-frontend/precache-manifest.da0759df0af54936a3a206b2da98b23d.js"
+  "/crm-frontend/precache-manifest.b3ea24da9fd89e4a9a9e67026edc5cbe.js"
 );
 
 workbox.clientsClaim();
