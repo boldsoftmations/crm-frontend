7d4219406f172dd69b01b7ce5105ea635bde7aa9
vivek-boldSoftMations <vivek@glutape.com>
2023-09-27 16:06:49 +0530

    fix : remove unassigned , dropped, duplicate leads to sales deputy manager

--- src/Components/Header/ListItems.jsx
+++ src/Components/Header/ListItems.jsx
@@ -3023,45 +3023,6 @@ export const ListItems = (props) => {
                       primary="Hot Leads"
                     />
                   </ListItem>
-                  <ListItem
-                    button
-                    component={RouterLink}
-                    to="/leads/closed-lead"
-                    style={{ width: 300 }}
-                  >
-                    <ListItemText
-                      component={Button}
-                      onClick={() => setOpen(false)}
-                      inset
-                      primary="Dropped Leads"
-                    />
-                  </ListItem>
-                  <ListItem
-                    button
-                    component={RouterLink}
-                    to="/leads/duplicate-lead"
-                    style={{ width: 300 }}
-                  >
-                    <ListItemText
-                      component={Button}
-                      onClick={() => setOpen(false)}
-                      inset
-                      primary="Duplicate Leads"
-                    />
-                  </ListItem>
-                  <ListItem
-                    button
-                    component={RouterLink}
-                    to="/leads/view-unassigned-lead"
-                    style={{ width: 300 }}
-                  >
-                    <ListItemText
-                      component={Button}
-                      onClick={() => setOpen(false)}
-                      inset
-                      primary="Unassigned Leads"
-                    />
-                  </ListItem>
                 </List>
               </Collapse>
 
