d642302ff5e2952098c66113cd5a2988ddda2b19
vivek-boldSoftMations <vivek@glutape.com>
2023-10-21 14:05:07 +0530

    feat : added date orbirth in produle user with dateformate

--- src/Pages/Profile/UserProfile/UserProfileView.jsx
+++ src/Pages/Profile/UserProfile/UserProfileView.jsx
@@ -9,54 +9,30 @@ import { UserProfileUpdate } from "./UserProfileUpdate";
 export const UserProfileView = () => {
   const [openPopup, setOpenPopup] = useState(false);
   const [IDForEdit, setIDForEdit] = useState();
-  const [state, setState] = useState({
-    open: false,
-    userProfiles: [], // Change this to an array
-    searchQuery: "",
-  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [userProfiles, setUserProfiles] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     getAllUserProfileData();
   }, []);
 
   const getAllUserProfileData = async () => {
-    setState((prev) => ({ ...prev, open: true }));
+    setIsLoading(true);
     try {
       const response = await UserProfileService.getAllUserProfileData();
       if (response.data) {
-        setState((prev) => ({
-          ...prev,
-          userProfiles: response.data, // Assign the entire array here
-          open: false,
-        }));
+        setUserProfiles(response.data);
       }
     } catch (err) {
       console.error("error profile", err);
-      setState((prev) => ({ ...prev, open: false }));
+    } finally {
+      setIsLoading(false);
     }
   };
 
   const handleInputChange = (event) => {
-    setState((prev) => ({ ...prev, searchQuery: event.target.value }));
-  };
-
-  const styles = {
-    container: {
-      padding: "16px",
-      margin: "16px",
-      boxShadow: "0px 3px 6px #00000029",
-      borderRadius: "4px",
-      display: "flex",
-      flexDirection: "column",
-      backgroundColor: "rgb(255, 255, 255)",
-    },
-    header: {
-      textAlign: "left",
-      marginBottom: "1em",
-      fontSize: "24px",
-      color: "rgb(34, 34, 34)",
-      fontWeight: 800,
-    },
+    setSearchQuery(event.target.value);
   };
 
   const Tableheaders = [
@@ -65,26 +41,35 @@ export const UserProfileView = () => {
     "LAST NAME",
     "PERSONAL CONTACT",
     "PERSONAL EMAIL",
+    "DATE OF BIRTH",
     "DATE OF JOINING",
     "ACTION",
   ];
 
-  const shouldDisplayUserProfile = () => {
-    const searchString = state.searchQuery.toLowerCase();
-    return state.userProfiles.some((user) =>
-      Object.values(user.personal || {}).some((value) =>
-        String(value).toLowerCase().includes(searchString)
-      )
-    );
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const options = { day: "2-digit", month: "2-digit", year: "numeric" };
+    return new Intl.DateTimeFormat("en-GB", options).format(date);
   };
 
-  const data = state.userProfiles.map((user) => ({
+  const filteredUserProfiles = userProfiles.filter((user) =>
+    Object.values(user.personal || {}).some((value) =>
+      String(value).toLowerCase().includes(searchQuery.toLowerCase())
+    )
+  );
+
+  const data = filteredUserProfiles.map((user) => ({
     id: user.id,
     first_name: user.personal.first_name || "-",
     last_name: user.personal.last_name || "-",
-    phone_number: user.personal.contact || "-", // Adjust as per your actual data structure
+    phone_number: user.personal.contact || "-",
     personal_email: user.personal.email || "-",
-    date_of_joining: user.personal.date_of_joining || "-",
+    date_of_birth: user.personal.date_of_birth
+      ? formatDate(user.personal.date_of_birth)
+      : "-",
+    date_of_joining: user.personal.date_of_joining
+      ? formatDate(user.personal.date_of_joining)
+      : "-",
   }));
 
   const openInPopup = (item) => {
@@ -95,23 +80,21 @@ export const UserProfileView = () => {
 
   return (
     <>
-      <CustomLoader open={state.open} />
+      <CustomLoader open={isLoading} />
       <div style={styles.container}>
         <div style={{ display: "flex" }}>
           <div style={{ flexGrow: 0.9 }}>
             <CustomSearch
-              filterSelectedQuery={state.searchQuery}
+              filterSelectedQuery={searchQuery}
               handleInputChange={handleInputChange}
-              getResetData={() =>
-                setState((prev) => ({ ...prev, searchQuery: "" }))
-              }
+              getResetData={() => setSearchQuery("")}
             />
           </div>
           <div style={{ flexGrow: 2 }}>
             <h3 style={styles.header}>User Profile</h3>
           </div>
         </div>
-        {shouldDisplayUserProfile() ? (
+        {filteredUserProfiles.length > 0 ? (
           <CustomTable
             headers={Tableheaders}
             data={data}
@@ -135,4 +118,23 @@ export const UserProfileView = () => {
       </Popup>
     </>
   );
+};
+
+const styles = {
+  container: {
+    padding: "16px",
+    margin: "16px",
+    boxShadow: "0px 3px 6px #00000029",
+    borderRadius: "4px",
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "rgb(255, 255, 255)",
+  },
+  header: {
+    textAlign: "left",
+    marginBottom: "1em",
+    fontSize: "24px",
+    color: "rgb(34, 34, 34)",
+    fontWeight: 800,
+  },
 };
