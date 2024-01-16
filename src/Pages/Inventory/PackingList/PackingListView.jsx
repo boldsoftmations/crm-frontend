c4f11735bee973724e513c033e7d36f2974a0926
BSrushil <rushil@boldsoftmation.com>
2024-01-16 16:52:15 +0530

    Fix: Gave create grn permission to acconts (requested by nilesh sir)

--- src/Pages/Inventory/PackingList/PackingListView.jsx
+++ src/Pages/Inventory/PackingList/PackingListView.jsx
@@ -342,6 +342,7 @@ function Row({ row, openInPopup, handleCreateGrn, userData }) {
           {
             // Show Create GRN button if the user is in any of the specified groups
             (userData.groups.includes("Stores Delhi") ||
+              userData.groups.includes("Accounts") ||
               userData.groups.includes("Production Delhi") ||
               userData.groups.includes("Stores") ||
               userData.groups.includes("Director")) && (
