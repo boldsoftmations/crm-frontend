7766f1fb45eda127f898bbe4ccd12fd8045216af
boldsoftmations <vivek@glutape.com>
2024-05-15 10:52:49 +0530

    feat : added for physical inventory for production groups

--- src/Components/Header/ListItems.jsx
+++ src/Components/Header/ListItems.jsx
@@ -1074,6 +1074,21 @@ export const ListItems = (props) => {
                 <ListItemText primary="Inventory" />
               </ListItem>
 
+              {/* Physical Inventory */}
+              <ListItem
+                button
+                component={RouterLink}
+                to="/inventory/physical"
+                style={{ width: 300 }}
+                onClick={() => setOpen(false)}
+                selected={isActive("/inventory/physical")}
+              >
+                <ListItemIcon>
+                  <StorageIcon />
+                </ListItemIcon>
+                <ListItemText primary="Physical Inventory" />
+              </ListItem>
+
               {/* Tasks */}
               <ListItem
                 button
