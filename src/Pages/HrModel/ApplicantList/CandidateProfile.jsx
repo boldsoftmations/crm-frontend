dc302f5dbbfbca9567b7610a60e1fc73dd83de0f
boldsoftmations <vivek@glutape.com>
2024-08-14 17:52:03 +0530

    feat: updated candidate profile ui

--- src/Pages/HrModel/ApplicantList/CandidateProfile.jsx
+++ src/Pages/HrModel/ApplicantList/CandidateProfile.jsx
@@ -11,11 +11,12 @@ import {
   IconButton,
   Button,
   Tooltip,
+  Chip,
 } from "@mui/material";
 import PersonIcon from "@mui/icons-material/Person";
 import EditIcon from "@mui/icons-material/Edit";
 import DownloadIcon from "@mui/icons-material/Download";
-import { blue } from "@mui/material/colors";
+import { blue, grey } from "@mui/material/colors";
 import Hr from "../../../services/Hr";
 import { CustomLoader } from "../../../Components/CustomLoader";
 import { Popup } from "../../../Components/Popup";
@@ -32,6 +33,7 @@ const CandidateProfile = ({ candidateData }) => {
     severity: "",
     open: false,
   });
+
   const handleClose = () => {
     setAlertMsg({ open: false });
   };
@@ -60,7 +62,6 @@ const CandidateProfile = ({ candidateData }) => {
     try {
       setLoader(true);
       const response = await Hr.getCandidateProfile(candidateData.id);
-      console.log("response", response.data);
       setCandidate(response.data);
     } catch (err) {
       console.log("error", err);
@@ -82,99 +83,150 @@ const CandidateProfile = ({ candidateData }) => {
         onClose={handleClose}
       />
       <CustomLoader open={loader} />
-      <Card sx={{ boxShadow: 3 }}>
+      <Card
+        sx={{
+          boxShadow: 3,
+          borderRadius: 3,
+          overflow: "hidden",
+          marginBottom: 4,
+        }}
+      >
         <CardContent>
-          <Grid container spacing={2} alignItems="start">
-            <Grid item xs={12} md={1}>
-              <Avatar sx={{ bgcolor: blue[500], width: 56, height: 56 }}>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={12} md={2} display="flex" justifyContent="center">
+              <Avatar
+                sx={{
+                  bgcolor: blue[500],
+                  width: 50,
+                  height: 50,
+                  fontSize: 40,
+                }}
+              >
                 <PersonIcon sx={{ fontSize: 40 }} />
               </Avatar>
             </Grid>
-            <Grid item xs={12} md={3} style={{ textAlign: "center" }}>
-              <Typography variant="h6">{candidate.name}</Typography>
+            <Grid item xs={12} md={4}>
+              <Typography variant="h5" fontWeight="bold">
+                {candidate.name || "N/A"}
+              </Typography>
               <Typography color="textSecondary">
-                {candidate.designation}
+                {candidate.designation || "No Designation"}
               </Typography>
             </Grid>
-            <Grid item xs={12} md={3} style={{ textAlign: "center" }}>
-              <Typography variant="h6">Status</Typography>
-              <Typography color="textSecondary">{candidate.status}</Typography>
+            <Grid
+              item
+              xs={12}
+              md={4}
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Box textAlign="center" width="50%">
+                <Typography variant="body1" color="textSecondary">
+                  Stage
+                </Typography>
+                <Chip
+                  label={candidate.stage || "Screening"}
+                  color="primary"
+                  variant="outlined"
+                  sx={{ fontWeight: "bold", mt: 1 }}
+                />
+              </Box>
+              <Box textAlign="center" width="50%">
+                <Typography variant="body1" color="textSecondary">
+                  Status
+                </Typography>
+                <Chip
+                  label={candidate.status || "Open"}
+                  color="secondary"
+                  variant="outlined"
+                  sx={{ fontWeight: "bold", mt: 1 }}
+                />
+              </Box>
             </Grid>
-            <Grid item xs={12} md={3} style={{ textAlign: "center" }}>
-              <Typography variant="h6">Stage</Typography>
-              <Typography color="textSecondary">{candidate.stage}</Typography>
-            </Grid>
-            <Grid item xs={12} md={2} style={{ textAlign: "end" }}>
+            <Grid item xs={12} md={2} mt={3}>
               <Tooltip title="Edit Profile">
-                <IconButton onClick={() => setOpenCandidatePopup(true)}>
+                <IconButton
+                  onClick={() => setOpenCandidatePopup(true)}
+                  sx={{ bgcolor: grey[200] }}
+                >
                   <EditIcon />
                 </IconButton>
               </Tooltip>
             </Grid>
           </Grid>
-          <Divider sx={{ my: 2 }} />
-          <Box>
-            <Typography variant="body2" sx={{ mb: 1 }}>
-              <strong>Email:</strong> {candidate.email}
-            </Typography>
-            <Typography variant="body2" sx={{ mb: 1 }}>
-              <strong>Contact:</strong> {candidate.contact}
-            </Typography>
-            <Typography variant="body2" sx={{ mb: 1 }}>
-              <strong>Department:</strong> {candidate.department}
-            </Typography>
-            <Typography variant="body2" sx={{ mb: 1 }}>
-              <strong>Job ID:</strong> {candidate.job}
-            </Typography>
-            <Typography variant="body2" sx={{ mb: 1 }}>
-              <strong>Location:</strong> {candidate.location}
-            </Typography>
-            <Typography variant="body2" sx={{ mb: 1 }}>
-              <strong>Current Location:</strong> {candidate.current_location}
-            </Typography>
-            <Typography variant="body2" sx={{ mb: 1 }}>
-              <strong>Current Salary:</strong> {candidate.current_salary}
-            </Typography>
-            <Typography variant="body2" sx={{ mb: 1 }}>
-              <strong>Expected Salary:</strong> {candidate.expected_salary}
-            </Typography>
-            <Typography variant="body2" sx={{ mb: 1 }}>
-              <strong>Spoken English:</strong> {candidate.spoken_english}
-            </Typography>
-            <Typography variant="body2" sx={{ mb: 1 }}>
-              <strong>Qualification:</strong> {candidate.qualification}
-            </Typography>
-            <Typography variant="body2" sx={{ mb: 1 }}>
-              <strong>Source:</strong> {candidate.source}
-            </Typography>
-            <Typography variant="body2" sx={{ mb: 1 }}>
-              <strong>Stage:</strong> {candidate.stage}
-            </Typography>
-            <Typography variant="body2" sx={{ mb: 1 }}>
-              <strong>Created By:</strong> {candidate.created_by}
-            </Typography>
-            <Box display="flex" gap="20px">
-              <Box>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  startIcon={<DownloadIcon />}
-                  onClick={handleDownload}
-                  style={{ marginTop: "20px" }}
-                  target="_blank"
-                  size="small"
-                >
-                  View CV
-                </Button>
-              </Box>
-
-              <Box style={{ marginTop: "20px" }}>
-                <UploadCv
-                  candidate={candidate}
-                  onUploadSuccess={getCandidateProfile}
-                />
-              </Box>
-            </Box>
+          <Divider sx={{ my: 3 }} />
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <Typography variant="body2" sx={{ mb: 1 }}>
+                <strong>Email:</strong> {candidate.email || "N/A"}
+              </Typography>
+              <Typography variant="body2" sx={{ mb: 1 }}>
+                <strong>Contact:</strong> {candidate.contact || "N/A"}
+              </Typography>
+              <Typography variant="body2" sx={{ mb: 1 }}>
+                <strong>Department:</strong> {candidate.department || "N/A"}
+              </Typography>
+              <Typography variant="body2" sx={{ mb: 1 }}>
+                <strong>Job ID:</strong> {candidate.job || "N/A"}
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Typography variant="body2" sx={{ mb: 1 }}>
+                <strong>Location:</strong> {candidate.location || "N/A"}
+              </Typography>
+              <Typography variant="body2" sx={{ mb: 1 }}>
+                <strong>Current Location:</strong>{" "}
+                {candidate.current_location || "N/A"}
+              </Typography>
+              <Typography variant="body2" sx={{ mb: 1 }}>
+                <strong>Current Salary:</strong>{" "}
+                {candidate.current_salary || "N/A"}
+              </Typography>
+              <Typography variant="body2" sx={{ mb: 1 }}>
+                <strong>Expected Salary:</strong>{" "}
+                {candidate.expected_salary || "N/A"}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid container spacing={2} sx={{ mt: 3 }}>
+            <Grid item xs={12} sm={6}>
+              <Typography variant="body2" sx={{ mb: 1 }}>
+                <strong>Spoken English:</strong>{" "}
+                {candidate.spoken_english || "N/A"}
+              </Typography>
+              <Typography variant="body2" sx={{ mb: 1 }}>
+                <strong>Qualification:</strong>{" "}
+                {candidate.qualification || "N/A"}
+              </Typography>
+              <Typography variant="body2" sx={{ mb: 1 }}>
+                <strong>Source:</strong> {candidate.source || "N/A"}
+              </Typography>
+              <Typography variant="body2" sx={{ mb: 1 }}>
+                <strong>Created By:</strong> {candidate.created_by || "N/A"}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Box
+            display="flex"
+            justifyContent="flex-start"
+            alignItems="center"
+            gap="20px"
+            sx={{ mt: 3 }}
+          >
+            <Button
+              variant="contained"
+              color="primary"
+              startIcon={<DownloadIcon />}
+              onClick={handleDownload}
+              size="small"
+            >
+              View CV
+            </Button>
+            <UploadCv
+              candidate={candidate}
+              onUploadSuccess={getCandidateProfile}
+            />
           </Box>
         </CardContent>
       </Card>
