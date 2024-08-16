f75e41c03735987aae6ee8ab3ae8467bf9c5778e
boldsoftmations <vivek@glutape.com>
2024-08-16 18:27:05 +0530

    fix: gave access to abd

--- src/Pages/Dispatch/AllDispatchTabView.jsx
+++ src/Pages/Dispatch/AllDispatchTabView.jsx
@@ -11,7 +11,7 @@ export const AllDispatchTabView = () => {
   const isInGroups = (...groups) =>
     groups.some((group) => userData.groups.includes(group));
 
-  const allTabs = isInGroups("Director", "Accounts");
+  const allTabs = isInGroups("Director", "Accounts","Accounts Billing Department");
 
   const twoTabs = isInGroups(
     "Factory-Delhi-Dispatch",
