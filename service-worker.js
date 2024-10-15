c47ffcae73a8821b1c8f5d8066063570d7e14034
boldsoftmations <vivek@glutape.com>
2024-10-15 11:40:21 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/crm-frontend/precache-manifest.2e9eea1cb673025fc34f23bf1029789e.js"
+  "/crm-frontend/precache-manifest.2ebea4775900cbd54742c691369dfd9f.js"
 );
 
 workbox.clientsClaim();
