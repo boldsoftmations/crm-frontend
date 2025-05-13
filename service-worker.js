d605e0965c25b77ef78d1c213f0d22c6a6a646fc
Aditya <vivek@glutape.com>
2025-05-13 12:42:53 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/crm-frontend/precache-manifest.c8544d6d918ea42b8d78004c4a02deb3.js"
+  "/crm-frontend/precache-manifest.74e020dd5297f8bbd6a5ee452da6f84b.js"
 );
 
 workbox.clientsClaim();
