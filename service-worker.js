b0a3673f5445e68b7d41913fb471b97547a9f0fa
Abhishekgit2004 <ap5012759@gmail.com>
2026-02-17 12:08:34 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/crm-frontend/precache-manifest.a9b7189f164b7c9168508127ebdc8e97.js"
+  "/crm-frontend/precache-manifest.7b1d12cbc556b5ceb3a2561ad16875e8.js"
 );
 
 workbox.clientsClaim();
