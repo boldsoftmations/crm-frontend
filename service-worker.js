f8139d1fc30771bf0b495640bc2bc399a64fc420
boldsoftmations <it2@glutape.com>
2026-02-18 10:24:27 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/crm-frontend/precache-manifest.7b1d12cbc556b5ceb3a2561ad16875e8.js"
+  "/crm-frontend/precache-manifest.3729f67a86b0160871ad7130d4a5376b.js"
 );
 
 workbox.clientsClaim();
