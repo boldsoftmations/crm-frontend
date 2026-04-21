9745b7d25299a89596e2c49b615d703a8f16075e
boldsoftmations <it2@glutape.com>
2026-04-21 15:21:34 +0530

    adding a decimal in forecast

--- src/Pages/ProductForecast/CurrentMonthForecastView.jsx
+++ src/Pages/ProductForecast/CurrentMonthForecastView.jsx
@@ -100,7 +100,6 @@ export const CurrentMonthForecastView = () => {
     "sales02@glutape.com",
     "bde03@glutape.com",
   ];
-
   const handleDownload = async () => {
     try {
       const data = await handleExport();
@@ -438,9 +437,18 @@ export const CurrentMonthForecastView = () => {
               </TableHead>
               <TableBody>
                 {currentMonthForecast.map((row, i) => {
-                  const sumValue = row.orderbook_value + row.actual;
-                  const forecast_achieved = row.forecast - sumValue;
-                  const rowColor = getColorForDate(row.anticipated_date); // Get the color for the date
+                  const sumValue =
+                    Number(row.orderbook_value) + Number(row.actual);
+
+                  const forecast_achieved = (
+                    Number(row.forecast) - sumValue
+                  ).toFixed(2);
+
+                  const rowColor = getColorForDate(row.anticipated_date);
+
+                  // Get the color for the date
+
+                  console.log(sumValue, forecast_achieved, rowColor);
                   return (
                     <StyledTableRow
                       key={row.id}
@@ -458,13 +466,13 @@ export const CurrentMonthForecastView = () => {
                         {row.product}
                       </StyledTableCell>
                       <StyledTableCell align="center">
-                        {row.forecast}
+                        {row.forecast && row.forecast.toFixed(2)}
                       </StyledTableCell>
                       <StyledTableCell align="center">
-                        {row.actual}
+                        {row.actual && row.actual.toFixed(2)}
                       </StyledTableCell>
                       <StyledTableCell align="center">
-                        {row.orderbook_value}
+                        {row.orderbook_value && row.orderbook_value.toFixed(2)}
                       </StyledTableCell>
                       <StyledTableCell align="center">
                         {forecast_achieved > 0 ? forecast_achieved : 0}
