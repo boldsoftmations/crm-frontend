79fe8afb074fcd105ba68b6a5721411b61d099dc
boldsoftmations <it2@glutape.com>
2026-05-26 18:07:00 +0530

    Updates

---
+++ precache-manifest.4777542a142950f9ac970802574eb64c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "12c99a3d7268bfd6c10c",
+    "url": "/crm-frontend/static/css/main.9d54c492.chunk.css"
+  },
+  {
+    "revision": "12c99a3d7268bfd6c10c",
+    "url": "/crm-frontend/static/js/main.f509985e.chunk.js"
+  },
+  {
+    "revision": "f751a901324a6647d3a8",
+    "url": "/crm-frontend/static/js/runtime~main.387c37e2.js"
+  },
+  {
+    "revision": "83d8bdd8f366286176b0",
+    "url": "/crm-frontend/static/css/2.7058c66a.chunk.css"
+  },
+  {
+    "revision": "83d8bdd8f366286176b0",
+    "url": "/crm-frontend/static/js/2.9685e1d9.chunk.js"
+  },
+  {
+    "revision": "2d825f673c20770c3de9",
+    "url": "/crm-frontend/static/js/3.e615720b.chunk.js"
+  },
+  {
+    "revision": "7050757aa99f82cecfb5",
+    "url": "/crm-frontend/static/js/4.a5c1e35f.chunk.js"
+  },
+  {
+    "revision": "185e3428466aee59cf6faf01edf54dfd",
+    "url": "/crm-frontend/static/media/MSME.185e3428.jpeg"
+  },
+  {
+    "revision": "f0200bc99a295939b66b08b1fe461035",
+    "url": "/crm-frontend/static/media/glutape logo.f0200bc9.jpg"
+  },
+  {
+    "revision": "cc6c45777bc3ce4315ae1fbe24b1fba9",
+    "url": "/crm-frontend/static/media/allLogo.cc6c4577.jpg"
+  },
+  {
+    "revision": "ba6cbe1188164e4fdba00e0c569bd5ce",
+    "url": "/crm-frontend/static/media/LOGOS3.ba6cbe11.png"
+  },
+  {
+    "revision": "92da350d0151010c0b2e7f615099d927",
+    "url": "/crm-frontend/static/media/ISOLogo.92da350d.ico"
+  },
+  {
+    "revision": "293088e28ee37ad39c6c0572c29286e6",
+    "url": "/crm-frontend/static/media/ISO.293088e2.png"
+  },
+  {
+    "revision": "5ef5f772b0579320f5a789ab8cb39dd2",
+    "url": "/crm-frontend/static/media/browser.5ef5f772.cjs"
+  },
+  {
+    "revision": "2114de15d1d582ab5e94d5e42889e323",
+    "url": "/crm-frontend/static/media/ISO.2114de15.jpg"
+  },
+  {
+    "revision": "2a4fa97a3c99cdbd5c3544ea5515fdf2",
+    "url": "/crm-frontend/static/media/MAINTENANCE.2a4fa97a.gif"
+  },
+  {
+    "revision": "76acf9c6831915f66b7ce8e7854d7f1d",
+    "url": "/crm-frontend/index.html"
+  }
+];
